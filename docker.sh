6dedbe56bc520ca34d7accb37e045ccf13516df3
Poguri Diya <diya>
2026-02-24 17:59:57 +0530

    terraform

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ sudo xfs -growfs /var
 ## install docker
 sudo dnf install -y dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo docker-builds-plugin
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
+sudo usermod -aG docker ec2-user 
